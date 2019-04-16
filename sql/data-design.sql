0b2e6e637ada21171a91d49de32961d998f634ba
Jamie Amparan <jleespooks@gmail.com>
2019-04-16 16:19:58 -0600

    data-design proj erd posted needs a few tweaks

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -3,7 +3,7 @@ drop table if exists product;
 drop table if exists cart;
 
 
-create table product (
+create table product(
 	productId binary(16) not null,
 	productName varchar (128),
 	product varchar (64),
@@ -11,14 +11,14 @@ create table product (
 	primary key(productId)
 );
 
-create table profile (
+create table profile(
 	profileId binary(16) not null,
 	profileName varchar(16) not null,
 	profile varchar(64),
 	primary key(profileId)
 );
 
-create table cart (
+create table cart(
 cartId binary(16) not null,
 cartProfileId binary(16) not null,
 cartProductId varchar(64) not null,
